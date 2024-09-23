da1ee2ce7ce0788a52c8b2e3c41cbfbd578b2531
ansarialiakbar <ansarialiakbar2001@gmail.com>
2024-09-24 02:11:58 +0530

    configure auto import sort in eslit

--- README.md
+++ README.md
@@ -45,3 +45,9 @@
   @tailwind utilities;
 
 ````````````
+
+### Adding plugins and dependencies
+
+````````````````
+npm install @reduxjs/toolkit react-redux react-router-dom react-icons react-chartjs-2 chart.js daisyui axios react-hot-toast @tailwindcss/line-clamp
+````````````
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 
-import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>
+    
  
-)
+);
