0b634052b51a5dc3d9d66cc483381bd69d65587c
ansarialiakbar <ansarialiakbar2001@gmail.com>
2024-09-25 02:37:10 +0530

    started basic setup with redux toolkit

---
+++ src/Redux/Slices/AuthSlice.js
@@ -0,0 +1,17 @@
+// for authentication purpuose
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState ={
+    isLoggedIn: localStorage.getItem("isLoggedIn") || false,
+    role: localStorage.getItem("role") || " ",
+    data: localStorage.getItem("data") || {},
+
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+})
+
+export default authSlice.reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import App from './App.jsx'
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux';
 import { Toaster } from 'react-hot-toast'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter } from 'react-router-dom'
+import store from './Redux/store.js';
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-     <App />
-     <Toaster />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
     
  
 );
